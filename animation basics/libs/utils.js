0070c4ca7e2fbddca8e4487d1c9445c0bcf49893
jiangweixu <jiangweixu@tencent.com>
2018-12-21 15:42:23 +0800

    +add 03 canvas

--- animation basics/libs/utils.js
+++ animation basics/libs/utils.js
@@ -84,3 +84,28 @@ window.utils.parseColor = function (color, toNumber) {
         return color;
     }
 };
+
+/**
+ * Converts a color to the RGB string format: 'rgb(r,g,b)' or 'rgba(r,g,b,a)'
+ * @param {number|string} color
+ * @param {number}        alpha
+ * @return {string}
+ */
+window.utils.colorToRGB = function (color, alpha) {
+    //number in octal format or string prefixed with #
+    if (typeof color === 'string' && color[0] === '#') {
+        color = window.parseInt(color.slice(1), 16);
+    }
+    alpha = (alpha === undefined) ? 1 : alpha;
+    //parse hex values
+    var r = color >> 16 & 0xff,
+        g = color >> 8 & 0xff,
+        b = color & 0xff,
+        a = (alpha < 0) ? 0 : ((alpha > 1) ? 1 : alpha);
+    //only use 'rgba' if needed
+    if (a === 1) {
+        return "rgb("+ r +","+ g +","+ b +")";
+    } else {
+        return "rgba("+ r +","+ g +","+ b +","+ a +")";
+    }
+};
