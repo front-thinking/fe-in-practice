eca66dce9f914bb11b25234e231fab78708bf9a3
jiangweixu <jiangweixu@tencent.com>
2018-12-25 17:40:33 +0800

    +add interaction

--- animation basics/libs/utils.js
+++ animation basics/libs/utils.js
@@ -109,3 +109,17 @@ window.utils.colorToRGB = function (color, alpha) {
         return "rgba("+ r +","+ g +","+ b +","+ a +")";
     }
 };
+
+/**
+ * Determine if a rectangle contains the coordinates (x,y) within it's boundaries.
+ * @param {object}  rect  Object with properties: x, y, width, height.
+ * @param {number}  x     Coordinate position x.
+ * @param {number}  y     Coordinate position y.
+ * @return {boolean}
+ */
+window.utils.containsPoint = function (rect, x, y) {
+    return !(x < rect.x ||
+        x > rect.x + rect.width ||
+        y < rect.y ||
+        y > rect.y + rect.height);
+};
