615cb38c68ad27346a749de88eaa7c84dc9298c2
jiangweixu <jiangweixu@tencent.com>
2018-12-26 20:12:30 +0800

    +add collision

--- animation basics/libs/utils.js
+++ animation basics/libs/utils.js
@@ -137,3 +137,16 @@ window.utils.containsPoint = function (rect, x, y) {
         y < rect.y ||
         y > rect.y + rect.height);
 };
+
+/**
+ * Determine if two rectangles overlap.
+ * @param {object}  rectA Object with properties: x, y, width, height.
+ * @param {object}  rectB Object with properties: x, y, width, height.
+ * @return {boolean}
+ */
+window.utils.intersects = function (rectA, rectB) {
+    return !(rectA.x + rectA.width < rectB.x ||
+        rectB.x + rectB.width < rectA.x ||
+        rectA.y + rectA.height < rectB.y ||
+        rectB.y + rectB.height < rectA.y);
+};
